77b0a77e58338ed353ea0e12dba43a146941ddbe
Daniel Santos <contatodanieljob@gmail.com>
2022-08-04 17:23:57 -0300

    feat(timeline): add nodes and icons

---
+++ components/TimelineIndicator/index.tsx
@@ -0,0 +1,21 @@
+import { IconProps, keyframes } from '@chakra-ui/react'
+import { FocusedIndicator } from '../../icons/FocusedIndicator'
+import { UnfocusedIndicator } from '../../icons/UnfocusedIndicator'
+
+const circleKeyframes = keyframes`
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+`
+
+const animation = `${circleKeyframes} 8s linear infinite`
+
+type Props = IconProps & {
+  focused: boolean
+}
+
+export const TimelineIndicator = ({ focused, ...props }: Props) => {
+
+  if (focused) return <FocusedIndicator animation={animation} {...props} />
+
+  return <UnfocusedIndicator {...props} />
+}
