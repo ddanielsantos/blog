95bad399c413e156d481c0ccd9e58e932eff753d
Daniel Santos <contatodanieljob@gmail.com>
2022-08-15 17:15:38 -0300

    styles: improve sizes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
 import { useState, useEffect } from 'react'
 import { COLORS } from '../theme'
-import { Text, Flex, Box } from '@chakra-ui/react'
+import { Text, Flex } from '@chakra-ui/react'
 import { Timeline } from '../components/Timeline'
+import { JobResume } from '../components/JobResume'
 
 export default function Home() {
   const [bgColor, setBgColor] = useState(COLORS.backgrounds.yellow)
@@ -41,15 +42,28 @@ export default function Home() {
         flexDir={'column'}
         alignItems='center'
 
-        scrollSnapType={'y mandatory'}
+        scrollSnapType={['none', 'none', 'y proximity']}
         overflowY='scroll'
         h='100vh'
+
+        css={{
+          '&::-webkit-scrollbar': {
+            width: '10px',
+          },
+          '&::-webkit-scrollbar-track': {
+            width: '6px',
+          },
+          '&::-webkit-scrollbar-thumb': {
+            background: '#ffffffcc',
+            borderRadius: '6px',
+          },
+        }}
       >
         <Flex
           as={'section'}
           bg={COLORS.backgrounds.yellow}
           shadow={'2xl'}
-          w={['100%', '48em']}
+          w={['100%', '100%', '100%', '60em']}
           flexDir='column'
           alignItems='flex-end'
           justifyContent={'flex-end'}
@@ -83,71 +97,77 @@ export default function Home() {
             fontSize={['1.25em', '1.25em']}
             textAlign='right'
           >
-            PASSIONATE FULLSTACK DEVELOPER
+            PASSIONATE DEVELOPER
           </Text>
         </Flex>
 
         <Flex
+          as='section'
           bg={COLORS.backgrounds.blue}
           shadow={'2xl'}
-          w={['100%', '48em']}
+          w={['100%', '100%', '100%', '60em']}
           flexDir='column'
           alignItems='flex-start'
           justifyContent={'flex-start'}
-          padding={'1em'}
 
+          p={['1rem', '1rem', '2rem']}
           flexShrink={0}
           scrollSnapAlign='start'
-          h={'100vh'}
+          minH={'100vh'}
         >
           <Text
-            lineHeight={1}
+          lineHeight={1}
             color={COLORS.paneIndicator.main.blue}
             textTransform={'uppercase'}
             fontWeight='extrabold'
-            fontSize={['3.5em', '4em']}
+            fontSize={['2.5em', '3em', '3.5em']}
           >
             me
           </Text>
         </Flex>
 
         <Flex
+          as='section'
           bg={COLORS.backgrounds.red}
           shadow={'2xl'}
-          w={['100%', '48em']}
+          w={['100%', '100%', '100%', '60em']}
           flexDir='column'
           alignItems='flex-start'
           justifyContent={'flex-start'}
 
+          p={['1rem', '1rem', '2rem']}
+
           flexShrink={0}
+          overflow='hidden'
           scrollSnapAlign='start'
-          h={'100vh'}
+          minH={'100vh'}
         >
-          <Flex
-            alignItems={'center'}
-            overflow={'hidden'}
+          <Text
+            lineHeight={1}
+            flexShrink={0}
+            color={COLORS.paneIndicator.main.red}
+            textTransform={'uppercase'}
+            fontWeight='extrabold'
+            fontSize={['2.5em', '3em', '3.5em']}
           >
-            <Timeline>
-              <Box>
-                pisadinha
-              </Box>
-              <Box>
-                caneta azul
-              </Box>
-              <Box>
-                jailson mendes
-              </Box>
-              <Box>
-                cavalo
-              </Box>
-              <Box>
-                irineu
-              </Box>
-              <Box>
-                voce nao sabe nem eu
-              </Box>
-            </Timeline>
-          </Flex>
+            Previous works
+          </Text>
+          <Timeline>
+            <JobResume
+              title='Lorem ipsum'
+              role='intern'
+              resume='Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.'
+              tags={['sql', 'nodejs']}
+              color={COLORS.paneIndicator.main.red}
+            />
+            <JobResume
+              title={'Noah\'s ark'}
+              role='intern'
+              resume={'Friday night tryna make it into the city Breakneck speeds, passenger seat something pretty Thinking back to how I got here in the first place Second class bitches wouldn\'t let me on first base A backpack  with luxury taste buds'}
+              tags={['sql', 'nodejs', 'sap', 'mongodb', 'graphql', 'irineu', 'weebs']}
+              color={COLORS.paneIndicator.main.red}
+            />
+          </Timeline>
         </Flex>
       </Flex>
     </>
