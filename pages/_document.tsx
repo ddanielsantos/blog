9267de545acb116a4aaa552ce32752f87fccc9bd
ddanielsantos <contatodanieljob@gmail.com>
2022-09-03 13:52:48 -0300

    feat: add custom fonts

---
+++ pages/_document.tsx
@@ -0,0 +1,20 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head />
+        <link
+          rel="preload"
+          href="/fonts/Virgil.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
