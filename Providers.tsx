1e0dc89c8123c3b3b6af77af258e2ab7e11a77fc
Daniel Santos <80872981+ddanielsantos@users.noreply.github.com>
2022-09-03 13:54:12 -0300

    feat: add custom fonts (#5)

--- Providers.tsx
+++ Providers.tsx
@@ -1,9 +1,24 @@
 import React from 'react'
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+import { Global } from '@emotion/react'
+
+const Fonts = () => (
+  <Global
+    styles={`
+      @font-face {
+        font-family: 'Virgil';
+        font-style: normal;
+        font-weight: 700;
+        src: url(/fonts/Virgil.woff2) format('woff2')
+      }
+    `}
+  />
+)
 
 export function Providers({ children }) {
   return (
     <ChakraProvider>
+    <Fonts/>
       {children}
     </ChakraProvider>
   )
--- next.config.js
+++ next.config.js
@@ -2,6 +2,19 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  headers: async () => {
+    return [
+      {
+        source: '/fonts/Virgil.woff2',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable'
+          }
+        ]
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
