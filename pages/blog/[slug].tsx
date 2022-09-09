7f0f35b1e0e4ab02663bbe29eedff5b9e2156007
ddanielsantos <contatodanieljob@gmail.com>
2022-09-08 23:03:14 -0300

    feat: generate static pages for /blog/[slug]

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,9 +1,35 @@
-const BlogPost = () => {
+import { GetStaticPaths, GetStaticProps } from "next"
+import { getPostsFilenames } from "../../src/lib/getPostsFileNames"
+import { getSlugFromFilename } from "../../src/lib/getSlugFromFilename"
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const filenames = getPostsFilenames()
+
+  return {
+    paths: filenames.map(name => {
+      return {
+        params: { slug: getSlugFromFilename(name) }
+      }
+    }),
+    fallback: false
+  }
+}
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const { params } = context
+  const { slug } = params
+
+  return {
+    props: {
+      data: { slug }
+    }
+  }
+}
+
+export default function BlogPost({ data }) {
   return (
     <h1>
-      coma batata
+      {data.slug}
     </h1>
   )
 }
-
-export default BlogPost
