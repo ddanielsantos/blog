9bd9ee3212ce6bf8569b5bc0d9ba78b145551056
ddanielsantos <contatodanieljob@gmail.com>
2022-09-13 13:11:23 -0300

    feat: pass metadata to next page

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -3,10 +3,10 @@ import emoji from 'remark-emoji'
 import dynamic from 'next/dynamic'
 import ReactMarkdown from 'react-markdown'
 import { getPost } from "../../src/lib/getPost"
-import { GetStaticPaths, GetStaticProps } from "next"
 import { CodeComponent } from 'react-markdown/lib/ast-to-react'
 import { getPostsFilenames } from "../../src/lib/getPostsFileNames"
 import { getSlugFromFilename } from "../../src/lib/getSlugFromFilename"
+import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from "next"
 
 const Code = dynamic(() => import('./Code'), { ssr: false })
 
@@ -31,24 +31,25 @@ const CodeBlock: CodeComponent = ({ children, inline, className }) => {
     <Code language={language} children={String(children)}/>
   )
 }
+type PostData = Awaited<ReturnType<typeof getPost>>['data']
 
-export const getStaticProps: GetStaticProps = async (context) => {
+export const getStaticProps: GetStaticProps<{ data: { content: string, meta: PostData } }> = async (context) => {
   if(!context.params?.slug) {
     return {
       notFound: true
     }
   }
   
-  const { content } = await getPost(context.params.slug + '.md')
+  const { content, data } = await getPost(context.params.slug + '.md')
 
   return {
     props: {
-      data: { content }
+      data: { content, meta: data }
     }
   }
 }
 
-export default function BlogPost({ data }) {
+export default function BlogPost({ data }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <ReactMarkdown
       remarkPlugins={[emoji, github]}
