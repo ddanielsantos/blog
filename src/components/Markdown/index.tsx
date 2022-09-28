6893bc9d8b1c2d75167c52ecf566c555dd6be1cf
ddanielsantos <contatodanieljob@gmail.com>
2022-09-28 19:26:37 -0300

    refactor: move to lighter lib

--- src/components/Markdown/index.tsx
+++ src/components/Markdown/index.tsx
@@ -1,7 +1,6 @@
 import Image from 'next/image'
-import github from 'remark-gfm'
 import dynamic from 'next/dynamic'
-import ReactMarkdown from 'react-markdown'
+import ReactMarkdown from 'markdown-to-jsx'
 import { UnorderedList } from '@chakra-ui/react'
 
 const CodeBlock = dynamic(
@@ -19,34 +18,45 @@ type Props = {
 export default function Markdown(props: Props) {
 	return (
 		<ReactMarkdown
-			remarkPlugins={[github]}
-			components={{
-				code: CodeBlock,
-				img: props => {
-					return <Image src={props.src} alt={props.alt} />
-				},
-				ul: props => (
-					<UnorderedList mb={'1em'} fontFamily={'Titillium Web'}>
-						{props.children}
-					</UnorderedList>
-				),
-				h1: props => <MarkdownText {...props} variant={'h1'} />,
-				h2: props => <MarkdownText {...props} variant={'h2'} />,
-				p: props => <MarkdownText {...props} variant={'p'} />,
-				a: props => {
-					return (
-						<a
-							href={props.href}
-							target={'_blank'}
-							rel={'noreferrer'}
-							style={{
-								textDecoration: 'underline',
-								textDecorationThickness: '2px'
-							}}
-						>
-							{props.children}
-						</a>
-					)
+			options={{
+				overrides: {
+					h1: {
+						component: MarkdownText,
+						props: {
+							variant: 'h1'
+						}
+					},
+					h2: {
+						component: MarkdownText,
+						props: {
+							variant: 'h2'
+						}
+					},
+					p: {
+						component: MarkdownText,
+						props: {
+							variant: 'p'
+						}
+					},
+					a: {
+						props: {
+							rel: 'noreferrer',
+							target: '_blank',
+							style: {
+								textDecoration: 'underline'
+							}
+						}
+					},
+					img: {
+						component: Image
+					},
+					ul: {
+						component: UnorderedList,
+						props: {
+							mb: '1em'
+						}
+					},
+					code: CodeBlock
 				}
 			}}
 		>
