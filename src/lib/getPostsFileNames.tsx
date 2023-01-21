bcd34ff4db0bd8a030db72e2822f26c0beafd9b2
Daniel Santos <80872981+ddanielsantos@users.noreply.github.com>
2023-01-21 00:42:10 -0300

    refactor/use astro (#22)
    * refactor: delete nextjs files
    
    * refactor: deps and astro config files
    
    * feat: translate to astro

--- src/lib/getPostsFileNames.tsx
+++
@@ -1,8 +0,0 @@
-import fs from 'node:fs'
-import path from 'node:path'
-
-const contentPath = path.join(process.cwd(), 'content')
-
-export const getPostsFilenames = (): string[] => {
-	return fs.readdirSync(contentPath)
-}
