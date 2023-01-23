84e779040feb2282a59cee191c5fe5f117f4f82c
Daniel Santos <contatodanieljob@gmail.com>
2023-01-23 20:26:57 -0300

    feat: fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,25 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-import vercel from "@astrojs/vercel/serverless";
-
-// https://astro.build/config
 export default defineConfig({
+  vite: {
+    plugins: [rawFonts([".ttf"])],
+    optimizeDeps: { exclude: ["satori", "yoga-wasm-web"] },
+  },
   integrations: [tailwind()],
-  output: "server",
-  adapter: vercel()
 });
+
+function rawFonts(ext) {
+  return {
+    name: "vite-plugin-raw-fonts",
+    transform(_, id) {
+      if (ext.some((e) => id.endsWith(e))) {
+        const buffer = fs.readFileSync(id);
+        return {
+          code: `export default ${JSON.stringify(buffer)}`,
+          map: null,
+        };
+      }
+    },
+  };
+}
