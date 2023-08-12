26c9b64f007462a3a541f8a5bccef749f7bc4f17
Daniel Santos <80872981+ddanielsantos@users.noreply.github.com>
2023-08-11 21:11:50 -0300

    refactor: variable font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,14 @@
+const defaultSans = require('tailwindcss/defaultTheme').fontFamily.sans
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+            fontFamily: {
+                sans: ['Open Sans', ...defaultSans]
+            }
+        },
 	},
 	plugins: [],
 }
