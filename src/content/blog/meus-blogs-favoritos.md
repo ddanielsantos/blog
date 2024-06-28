6d74625ff1da0b9232bd763c9fe4b99eb4e20801
ddanielsantos <80872981+ddanielsantos@users.noreply.github.com>
2024-06-28 11:42:41 -0300

    feat: monthly tweaks

--- src/content/blog/meus-blogs-favoritos.md
+++ src/content/blog/meus-blogs-favoritos.md
@@ -5,12 +5,8 @@ date: "28 Sep, 2024"
 tags: ["list"]
 ---
 
-vou tentar manter essa lista atualizada
+vou tentar manter essa lista atualizada, são blogs que eu adoraria copiar na cara de pau
 
-benchmarks de blogs que eu adoraria copiar na cara de pau
-
-pra mim, os melhores são:
-
-- hillel wayne [https://www.hillelwayne.com/]
-- without boats [https://without.boats/]
-- matklad [https://matklad.github.io/]
+- hillel wayne https://www.hillelwayne.com/
+- without boats https://without.boats/
+- matklad https://matklad.github.io/
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,15 +1,15 @@
-const defaultSans = require('tailwindcss/defaultTheme').fontFamily.sans
+const defaultSans = require("tailwindcss/defaultTheme").fontFamily.sans;
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-            fontFamily: {
-                sans: ['Open Sans', ...defaultSans]
-            }
-        },
-	},
-	plugins: [],
-    darkMode: `class`
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Open Sans", ...defaultSans],
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+  darkMode: `class`,
+};
