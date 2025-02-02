84309a3caddbe340a9f5d4ffd0530cd024ba1dd3
ddanielsantos <80872981+ddanielsantos@users.noreply.github.com>
2025-02-02 17:56:51 -0300

    chore: fix versions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,45 +1,41 @@
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
-import react from '@astrojs/react';
-
+import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
-
 import tailwindcss from "@tailwindcss/vite";
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://www.ddaniel.me',
-  vite: {
-    plugins: [rawFonts([".ttf"]), tailwindcss()],
-    optimizeDeps: { exclude: ["@resvg/resvg-js"] },
-  },
-  integrations: [mdx(), react()],
-  markdown: {
-    shikiConfig: {
-      themes: { light: 'one-light', dark: 'min-dark' },
-      transformers: [
-        {
-          pre(node) {
-            delete node.properties.tabindex;
-          }
-        }
-      ]
-    },
-  }
+	site: "https://www.ddaniel.me",
+	vite: {
+		plugins: [rawFonts([".ttf"]), tailwindcss()],
+		optimizeDeps: { exclude: ["@resvg/resvg-js"] },
+	},
+	integrations: [mdx(), react()],
+	markdown: {
+		shikiConfig: {
+			themes: { light: "one-light", dark: "min-dark" },
+			transformers: [
+				{
+					pre(node) {
+						node.properties.tabindex = undefined;
+					},
+				},
+			],
+		},
+	},
 });
 
 function rawFonts(ext) {
-  return {
-    name: "vite-plugin-raw-fonts",
-    transform(_, id) {
-      if (ext.some((e) => id.endsWith(e))) {
-        const buffer = fs.readFileSync(id);
-        return {
-          code: `export default ${JSON.stringify(buffer)}`,
-          map: null,
-        };
-      }
-    },
-  };
+	return {
+		name: "vite-plugin-raw-fonts",
+		transform(_, id) {
+			if (ext.some((e) => id.endsWith(e))) {
+				const buffer = fs.readFileSync(id);
+				return {
+					code: `export default ${JSON.stringify(buffer)}`,
+					map: null,
+				};
+			}
+		},
+	};
 }
